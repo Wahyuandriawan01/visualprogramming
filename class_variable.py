6b801b614897396c41a11b68829eb5609f117212
Wahyuandriawan01 <101026787+Wahyuandriawan01@users.noreply.github.com>
2022-04-06 14:15:08 +0800

    Latihan Opp

--- class_variable.py
+++ class_variable.py
@@ -3,6 +3,7 @@ class keluarga:
       self.bapak = bapak
       self.ibu = ibu
       self.kakak = kakak
+      
 nama = keluarga('Muh','Nur','Wahyu Andriawan')
 #Untuk Mengakses class diatas caranya sebagai berikut:
 print(nama.bapak)
