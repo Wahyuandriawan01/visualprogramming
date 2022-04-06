99403a7c71ec1224c8f871850e84ff16ef93f2fd
Wahyuandriawan01 <101026787+Wahyuandriawan01@users.noreply.github.com>
2022-04-06 14:10:40 +0800

    OPP Latihan

---
+++ class_variable.py
@@ -0,0 +1,12 @@
+class keluarga:
+  def __init__(self, bapak, ibu, kakak):
+      self.bapak = bapak
+      self.ibu = ibu
+      self.kakak = kakak
+nama = keluarga('Muh','Nur','Wahyu Andriawan')
+#Untuk Mengakses class diatas caranya sebagai berikut:
+print(nama.bapak)
+print(nama.ibu)
+print(nama.kakak)
+
+
